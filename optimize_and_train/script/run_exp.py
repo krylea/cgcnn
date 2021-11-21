3923c1ebe12beb1d8a8cb65ef4af381a62a2822c
Kira Selby <kaselby@uwaterloo.ca>
2021-11-21 15:42:10 -0500

    run exp

--- optimize_and_train/script/run_exp.py
+++ optimize_and_train/script/run_exp.py
@@ -27,9 +27,9 @@ H_HYPERPARAMS = {
 }
 
 def parse_args():
-    parser = argparse.ArgumentParser
-    parser.add_argument('--adsorbate', choices=['co', 'h'], default='co')
-    parser.add_argument('--optim', choices=['sgd', 'adam'], default='sgd')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--adsorbate', type=str, choices=['co', 'h'], default='co')
+    parser.add_argument('--optim', type=str, choices=['sgd', 'adam'], default='sgd')
     parser.add_argument('--atom_fea_len', type=int, default=-1)
     parser.add_argument('--batch_size', type=int, default=-1)
     parser.add_argument('--disable_cuda', action='store_true')
