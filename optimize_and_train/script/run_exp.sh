f3259323db41b0962032859538945788411414cb
Kira Selby <kaselby@uwaterloo.ca>
2021-11-21 15:45:40 -0500

    sbatch

---
+++ optimize_and_train/script/run_exp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=test
+#SBATCH --output=logs/slurm-%j.txt
+#SBATCH --open-mode=append
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --partition=t4v1,t4v2,p100
+#SBATCH --cpus-per-gpu=1
+#SBATCH --mem=25GB
+#SBATCH --exclude=gpu109
+
+python3 run_exp.py --adsorbate $1 --root_dir "test-data/${1}/root_dir"
